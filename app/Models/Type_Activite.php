c0724cf25895dd150298d0977d836fb437ad8eeb
TheRe-Der <re.reder98@gmail.com>
2026-09-12 15:12:58 +0100

    feat: simple changes

--- app/Models/Type_Activite.php
+++ app/Models/Type_Activite.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Type_Activite extends Model
 {
+<<<<<<< HEAD
     protected $table = "type_activite";
+=======
+    protected $table = "type_Activite";
+>>>>>>> 6555ff0 (feat: change the models table names)
 
     protected $fillable = [
         "Libelle"
